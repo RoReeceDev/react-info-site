7e923f5f7dd4d731ea069997225ff1ac52ebb166
RoReeceDev <roreecedev@gmail.com>
2024-01-19 20:38:03 -0500

    set up react app and start project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import Main from './components/Main.jsx'
+
+
+const App = () => {
+  return(
+    <>
+    <Main />
+    <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Main = () =>{
+    return(
+    <h1>This is the Main</h1>
+    )
+}
+
+export default Main 
